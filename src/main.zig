d4206a94fce9e0cf22bc3450f69773b3039ae910
Wojciech Chuchro <82377967+WojciechChuchro@users.noreply.github.com>
2025-04-06 21:45:37 +0200

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,10 @@
 const std = @import("std");
 const net = std.net;
 
+fn sendNotFound(conn: net.Server.Connection) !void {
+    try conn.stream.writeAll("HTTP/1.1 404 Not Found\r\n\r\n", .{});
+}
+
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     try stdout.print("Logs from your program will appear here!\n", .{});
@@ -18,13 +22,37 @@ pub fn main() !void {
     
     defer alloc.free(input);
 
-    _ = try conn.stream.read(input);
+    const bytes_read = try conn.stream.read(input);
+    try stdout.print("-----REQUEST-----: \n{s}\n", .{input[0..bytes_read]});
     
-    var iter = std.mem.splitAny(u8, input, " ");
-    _ = iter.next();
+    var lines = std.mem.splitSequence(u8, input, "\n");
     
-    if (std.mem.eql(u8, iter.next().?, "/")) {
-        try success(conn);
+    const start_line = lines.next().?;
+    var s = std.mem.splitSequence(u8, start_line, " ");
+    
+    const method = s.next().?;
+    const request_target = s.next().?;
+    const protocol = s.next().?;
+    _ = method;
+    _ = protocol;
+    
+    try stdout.print("Path: \n{s}\n", .{request_target});
+    var url_parts = std.mem.splitAny(u8, request_target, "/");
+    const part1 = url_parts.next().?;
+    const part2 = url_parts.next().?;
+    const part3 = url_parts.next().?;
+    try stdout.print("part1: {s}\n", .{part1});
+    try stdout.print("part1: {s}\n", .{part2});
+    try stdout.print("part1: {s}\n", .{part3});
+    
+    if (std.mem.eql(u8, part1, "/")) {
+        if(std.mem.eql(u8, part2, "echo")) {
+            if(std.mem.eql(u8, part2, "abc")) {
+                
+            }
+        } else {
+            try success(conn);
+        }
     } else {
         try not_found(conn);
     }
@@ -32,6 +60,10 @@ pub fn main() !void {
 
 pub fn success(conn: net.Server.Connection) !void {
     _ = try conn.stream.write("HTTP/1.1 200 OK\r\n\r\n");
+}
+
+pub fn responseWithBody(conn: net.Server.Connection) !void {
+    _ = try conn.stream.write("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\nabc");
 }
 
 pub fn not_found(conn: net.Server.Connection) !void {
