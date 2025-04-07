8fc8f5a32a085da838aa9642b99848e77f013a8e
Wojciech Chuchro <82377967+WojciechChuchro@users.noreply.github.com>
2025-04-07 20:30:15 +0200

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -6,36 +6,46 @@ fn sendNotFound(conn: net.Server.Connection) !void {
 }
 
 pub fn main() !void {
+    const port = 4221;
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("Logs from your program will appear here!\n", .{});
-    const address = try net.Address.resolveIp("127.0.0.1", 4221);
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const response = try allocator.alloc(u8, 1024);
+    defer allocator.free(response);
+
+    //std.mem.copyBackwards(u8, response[0..str.len], str);
+
+    try stdout.print("Server started at port {}\n", .{port});
+
+    const address = try net.Address.resolveIp("127.0.0.1", port);
     var listener = try address.listen(.{
         .reuse_address = true,
     });
     defer listener.deinit();
-    
+
     const conn = try listener.accept();
     defer conn.stream.close();
-    
-    const alloc = std.heap.page_allocator;
-    const input = try alloc.alloc(u8, 1024);
-    
-    defer alloc.free(input);
+
+    const input = try allocator.alloc(u8, 1024);
+    defer allocator.free(input);
 
     const bytes_read = try conn.stream.read(input);
     try stdout.print("-----REQUEST-----: \n{s}\n", .{input[0..bytes_read]});
-    
+
     var lines = std.mem.splitSequence(u8, input, "\n");
-    
+
     const start_line = lines.next().?;
     var s = std.mem.splitSequence(u8, start_line, " ");
-    
+
     const method = s.next().?;
     const request_target = s.next().?;
     const protocol = s.next().?;
     _ = method;
     _ = protocol;
-    
+
     try stdout.print("Path: \n{s}\n", .{request_target});
     var url_parts = std.mem.splitAny(u8, request_target, "/");
     const part1 = url_parts.next().?;
@@ -44,13 +54,13 @@ pub fn main() !void {
     try stdout.print("part1: {s}\n", .{part1});
     try stdout.print("part1: {s}\n", .{part2});
     try stdout.print("part1: {s}\n", .{part3});
-    
+
     if (std.mem.eql(u8, part1, "")) {
-        if(part2.len > 0 and std.mem.eql(u8, part2, "echo")) {
-            if(part3.len > 0) {
-                
-                try stdout.print("hi \n", .{});
-                try responseWithBody(conn, part3);
+        if (std.mem.eql(u8, part2, "echo")) {
+            if (part3.len > 0) {
+                const message = try std.fmt.allocPrint(allocator, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {d}\r\n\r\n{s}", .{ part3.len, part3 });
+                defer allocator.free(message);
+                _ = try conn.stream.write(message);
             }
         } else {
             try success(conn);
@@ -64,7 +74,6 @@ pub fn responseWithBody(conn: net.Server.Connection, body: []const u8) !void {
     var header_buf: [128]u8 = undefined;
     const header = try std.fmt.bufPrint(
         &header_buf,
-        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n",
         .{body.len},
     );
 
@@ -74,7 +83,6 @@ pub fn responseWithBody(conn: net.Server.Connection, body: []const u8) !void {
 pub fn success(conn: net.Server.Connection) !void {
     _ = try conn.stream.write("HTTP/1.1 200 OK\r\n\r\n");
 }
-
 
 pub fn not_found(conn: net.Server.Connection) !void {
     _ = try conn.stream.write("HTTP/1.1 404 Not Found\r\n\r\n");
