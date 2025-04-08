20dcacdeaf073decff0662e78be7984a865a7023
Wojciech Chuchro <82377967+WojciechChuchro@users.noreply.github.com>
2025-04-08 18:42:11 +0200

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -36,6 +36,26 @@ pub fn main() !void {
     _ = try conn.stream.read(input);
 
     var iter = std.mem.splitAny(u8, input, " ");
+    var iter2 = std.mem.splitAny(u8, input, "\r\n");
+    var user_agent: ?[]const u8 = undefined;
+
+    try stdout.print("------------", .{});
+    const ua_placeholder = "User-Agent: ";
+
+    while (iter2.next()) |line| {
+        try stdout.print("val: {s} len {}\n", .{ line, line.len });
+        if (line.len == 0) continue;
+        const index = std.mem.indexOf(u8, line, ua_placeholder) != null;
+        if (index) {
+            // Found the User-Agent header
+            try stdout.print("Found User-Agent header\n", .{});
+
+            // Extract the value part (after "User-Agent:")
+            const value_start = ua_placeholder.len;
+            user_agent = std.mem.trim(u8, line[value_start..], " ");
+        }
+    }
+    try stdout.print("------------", .{});
 
     _ = iter.next();
 
@@ -56,6 +76,9 @@ pub fn main() !void {
 
         const res = try std.fmt.allocPrint(alloc, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {0d}\r\n\r\n{1s}", .{ first.?.len, first.? });
 
+        _ = try conn.stream.writeAll(res);
+    } else if (std.mem.eql(u8, root.?, "user-agent")) {
+        const res = try std.fmt.allocPrint(alloc, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {0d}\r\n\r\n{1s}", .{ user_agent.?.len, user_agent.? });
         _ = try conn.stream.writeAll(res);
     } else {
         _ = try conn.stream.writeAll("HTTP/1.1 404 Not Found\r\n\r\n");
